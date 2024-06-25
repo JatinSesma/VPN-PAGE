a71b4a0b93401b3c9099e27d14ac23b51192a26d
JatinSesma <gholujatt0.com>
2024-06-25 11:19:03 +0530

    section-5 complete

---
+++ assets/js/app.js
@@ -0,0 +1,37 @@
+$('.slick-slider').slick({
+    dots: true,
+    infinite: true,
+    prevArrow: ".prev",
+    nextArrow: ".next",
+    speed: 300,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    responsive: [
+        {
+            breakpoint: 992,
+            settings: {
+                slidesToShow: 2,
+                slidesToScroll: 1,
+                infinite: true,
+                dots: true
+            }
+        },
+        {
+            breakpoint: 768,
+            settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1
+            }
+        },
+        {
+            breakpoint: 320,
+            settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1
+            }
+        }
+        // You can unslick at a given breakpoint now by adding:
+        // settings: "unslick"
+        // instead of a settings object
+    ]
+});
