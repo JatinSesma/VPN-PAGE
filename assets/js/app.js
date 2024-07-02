1fcd4a41fffaac67892836aeec5e966ce9c0d7eb
JatinSesma <gholujatt0.com>
2024-07-02 09:43:15 +0530

    mistake update & nav-bar responsive done

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,3 +1,11 @@
+let navicon = document.querySelector(".nav-icon");
+let lgview = document.querySelector(".lg-view");
+navicon.addEventListener("click", () => {
+    lgview.classList.toggle("show");
+});
+
+
+
 $('.slick-slider').slick({
     dots: true,
     infinite: true,
